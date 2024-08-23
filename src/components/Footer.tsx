a1ac85ba99da6907c646bd057998058e4ddba7e1
sondoan17 <sondoan17@gmail.com>
2024-08-24 03:24:18 +0700

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <footer className='bg-white h-20 relative'>
             <MaxWidthWrapper>
                 <div className='border-t border-gray-200' />
-                <div className='h-full flex flex-col md:flex-row md:justify-between justify-center items-center'>
+                <div className='h-20 flex flex-col md:flex-row md:justify-between justify-center items-center'>
                     <div className='text-center md:text-left pb-2 md:pb-0'>
                         <p className='text-sm text-muted-foreground'>
                             &copy; {new Date().getFullYear()} All rights reserved
